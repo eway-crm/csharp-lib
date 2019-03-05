88b21252987f4cd9e658f419600c470d6686babd
Jiří Patera <jiri.patera@eway-crm.com>
2019-03-05 10:35:37 +0100

    refs #34377: Source code fix

---
+++ Examples/CreateDocument/Program.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Newtonsoft.Json.Linq;
+
+using eWayCRM.API;
+
+
+namespace ExamplesTesting.CreateDocument
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Connection to API
+            Connection wcfConnection = new Connection(
+                "https://trial.eway-crm.com/31994",
+                "api",
+                "470AE7216203E23E1983EF1851E72947",
+                "TestingConnector01"
+                );
+
+            // Create Document
+            wcfConnection.UploadFile(@"C:\Users\jpatera\Documents\File.txt", out Guid Guid);
+            wcfConnection.CallMethod("SaveDocument", JObject.FromObject(new
+            {
+                transmitObject = new
+                {
+                    ItemGUID = Guid,
+                    FileAs = "File.txt",
+                    DocName = "File",
+                    Extension = "txt",
+                    DocSize = 0
+                }
+            }));
+        }
+    }
+}
