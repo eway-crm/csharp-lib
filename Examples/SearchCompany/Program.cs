c0af5ee816d9ceeed53410264c587b1ce064f998
Jiří Patera <jiri.patera@eway-crm.com>
2019-03-15 15:24:13 +0100

    refs #35256: reflect on feedback

--- Examples/SearchCompany/Program.cs
+++ Examples/SearchCompany/Program.cs
@@ -31,6 +31,7 @@ namespace ExamplesTesting.SearchForCompany
                 }
             }));
 
+			// Output table
             Console.WriteLine("| {0,-35} | {1,-35} | {2,-35} |", "Name", "Address", "Phone");
             foreach (var item in ((JArray)response.GetValue("Data")))
             {
